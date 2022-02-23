f855a1dd9cecdb8488c5a881e5fb29dbda9cd254
Italo Maciel <italo.maciel.118@ufrn.edu.br>
2022-02-22 21:39:06 -0300

    Projeto Criado

---
+++ src/main/java/com/italomaciel/cursomc/PraticamcApplication.java
@@ -0,0 +1,13 @@
+package com.italomaciel.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticamcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/italomaciel/cursomc/PraticamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italomaciel.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticamcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
