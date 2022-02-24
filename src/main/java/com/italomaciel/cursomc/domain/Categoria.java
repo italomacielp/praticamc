8ad787e2e724beb070fedf03bba4e3e5cb2dfb08
Italo Maciel <italo.maciel.118@ufrn.edu.br>
2022-02-23 23:10:31 -0300

    Testando a primeira classe de Domínio - 'Categoria'

---
+++ src/main/java/com/italomaciel/cursomc/domain/Categoria.java
@@ -0,0 +1,59 @@
+package com.italomaciel.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//As categorias são transformadas em bits, para serem gravados em arquvios e etc.
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	//Compara por valor e não por ponteiro de memória
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
