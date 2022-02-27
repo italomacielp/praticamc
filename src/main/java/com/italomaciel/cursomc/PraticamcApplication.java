d149a27cfbc3af8db05dd11d673d7498097bfe59
Italo Maciel <italo.maciel.118@ufrn.edu.br>
2022-02-27 11:26:37 -0300

    Cliente, TipoCliente, telefones e endereços

--- src/main/java/com/italomaciel/cursomc/PraticamcApplication.java
+++ src/main/java/com/italomaciel/cursomc/PraticamcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.italomaciel.cursomc.domain.Categoria;
 import com.italomaciel.cursomc.domain.Cidade;
+import com.italomaciel.cursomc.domain.Cliente;
+import com.italomaciel.cursomc.domain.Endereco;
 import com.italomaciel.cursomc.domain.Estado;
 import com.italomaciel.cursomc.domain.Produto;
+import com.italomaciel.cursomc.domain.enums.TipoCliente;
 import com.italomaciel.cursomc.repositories.CategoriaRepository;
 import com.italomaciel.cursomc.repositories.CidadeRepository;
+import com.italomaciel.cursomc.repositories.ClienteRepository;
+import com.italomaciel.cursomc.repositories.EnderecoRepository;
 import com.italomaciel.cursomc.repositories.EstadoRepository;
 import com.italomaciel.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,10 @@ public class PraticamcApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired 
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PraticamcApplication.class, args);
@@ -67,7 +76,19 @@ public class PraticamcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-			
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
 	}
 
 	
---
+++ src/main/java/com/italomaciel/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,43 @@
+package com.italomaciel.cursomc.domain.enums;
+
+//Associamos um código numérico a uma enumeração evitando inconsistências futuras no banco de dados
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int codigo;
+	private String descricao;
+	
+	private TipoCliente(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	//Será capaz de comparar sem instancear. 
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id Inválido: " + cod);
+		
+	}
+	
+}
