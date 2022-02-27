d1544e44d70e25f6e7145581b0ddb9b2411b450d
Italo Maciel <italo.maciel.118@ufrn.edu.br>
2022-02-27 01:41:09 -0300

    Estado e Cidade

--- src/main/java/com/italomaciel/cursomc/PraticamcApplication.java
+++ src/main/java/com/italomaciel/cursomc/PraticamcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.italomaciel.cursomc.domain.Categoria;
+import com.italomaciel.cursomc.domain.Cidade;
+import com.italomaciel.cursomc.domain.Estado;
 import com.italomaciel.cursomc.domain.Produto;
 import com.italomaciel.cursomc.repositories.CategoriaRepository;
+import com.italomaciel.cursomc.repositories.CidadeRepository;
+import com.italomaciel.cursomc.repositories.EstadoRepository;
 import com.italomaciel.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,10 @@ public class PraticamcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired 
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PraticamcApplication.class, args);
@@ -47,6 +55,19 @@ public class PraticamcApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2)); //Arrays.asList cria uma lista automaticamente.
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+			
 	}
 
 	
